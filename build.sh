1ae96abb042d0726cb1d1695f253166b07814695
henning <henning.woydt@gmail.com>
2024-10-11 10:46:26 +0200

    * update build script

--- build.sh
+++ build.sh
@@ -6,20 +6,21 @@ function get_num_cores {
 
 ROOT=${PWD}
 if [ -d .git ]; then
-  # SharedMap is build from a git repository
+#  SharedMap is build from a git repository
   git submodule update --init --recursive
 fi
 
-
-cd extern && ./install_mtkahypar.sh
+# install mt_kahypar
+mkdir extern/mt_kahypar_local
+mkdir extern/mt-kahypar/build
+cd extern/mt-kahypar/build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DKAHYPAR_DOWNLOAD_TBB=ON -DMT_KAHYPAR_DISABLE_BOOST=ON -DKAHYPAR_ENABLE_THREAD_PINNING=OFF -DKAHYPAR_DISABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
+make -j16 install.mtkahypar
 cd ${ROOT}
 
-cd extern/ProcessMappingVerifier && ./build.sh
-cd ${ROOT}
-
+# build SharedMap
 mkdir build
 cd build
-
-cmake .. -DCMAKE_BUILD_TYPE=Release -DMTRECPROMAP_ENABLE_ASSERTIONS=OFF -DMTRECPROMAP_ENABLE_STATCOLLECTOR=OFF && cd ${ROOT}
-cmake --build build --parallel "$(get_num_cores)" --target recpromap
+cmake .. -DCMAKE_BUILD_TYPE=Release && cd ${ROOT}
+cmake --build build --parallel "$(get_num_cores)" --target SharedMap
 cd ${ROOT}
