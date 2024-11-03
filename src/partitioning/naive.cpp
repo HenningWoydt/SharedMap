5c83b77d76e9287f78917e51ae8e0f516bd7d039
henning <henning.woydt@gmail.com>
2024-11-03 17:31:31 +0100

    * added some definitions

--- src/partitioning/naive.cpp
+++ src/partitioning/naive.cpp
@@ -1,3 +1,12 @@
 #include "naive.h"
 
-namespace SharedMap {}
+#include "src/datastructures/translation_table.h"
+
+namespace SharedMap {
+    std::vector<u64> solve_naive(const Graph& original_g, const AlgorithmConfiguration& config, StatCollector& stat_collector) {
+        std::vector<u64> partition(original_g.get_n()); // end partition
+        TranslationTable original_tt(original_g.get_n()); // default translation table
+
+        return partition;
+    }
+}
--- src/partitioning/nb_layer.cpp
+++ src/partitioning/nb_layer.cpp
@@ -1,7 +1,12 @@
 #include "nb_layer.h"
 
+#include "src/datastructures/translation_table.h"
+
 namespace SharedMap {
     std::vector<u64> solve_nb_layer(const Graph& original_g, const AlgorithmConfiguration& config, StatCollector& stat_collector) {
+        std::vector<u64> partition(original_g.get_n()); // end partition
+        TranslationTable original_tt(original_g.get_n()); // default translation table
 
+        return partition;
     }
 }
