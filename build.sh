7d667e13de44ed1fdb7608f244d2346ae54447fe
henning <henning.woydt@gmail.com>
2025-01-29 15:18:18 +0100

    * binary placed in own folder * added binary folder to gitignore

--- build.sh
+++ build.sh
@@ -11,16 +11,19 @@ if [ -d .git ]; then
 fi
 
 # install mt_kahypar
-mkdir extern/mt_kahypar_local
-mkdir extern/mt-kahypar/build
+mkdir -p extern/mt_kahypar_local
+mkdir -p extern/mt-kahypar/build
 cd extern/mt-kahypar/build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DKAHYPAR_DOWNLOAD_TBB=ON -DMT_KAHYPAR_DISABLE_BOOST=ON -DKAHYPAR_ENABLE_THREAD_PINNING=OFF -DKAHYPAR_DISABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
 make -j"$(get_num_cores)" install.mtkahypar
 cd ${ROOT}
 
 # build SharedMap
-mkdir build
+mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release && cd ${ROOT}
 cmake --build build --parallel "$(get_num_cores)" --target SharedMap
 cd ${ROOT}
+
+mkdir -p bin
+mv build/SharedMap bin/SharedMap
