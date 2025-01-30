cc0fb0fb1995ef690478ec8ac4cc75835e1db3c1
henning <henning.woydt@gmail.com>
2025-01-30 15:08:16 +0100

    * removed intel tbb from linking

--- build.sh
+++ build.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-function get_num_cores {
-  grep -c ^processor /proc/cpuinfo;
-}
-
 ROOT=${PWD}
 if [ -d .git ]; then
 #  SharedMap is build from a git repository
@@ -16,7 +12,7 @@ mkdir -p extern/mt_kahypar_local
 mkdir -p extern/mt-kahypar/build
 cd extern/mt-kahypar/build
 cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DKAHYPAR_DOWNLOAD_TBB=ON -DKAHYPAR_DOWNLOAD_BOOST=ON -DKAHYPAR_ENABLE_THREAD_PINNING=OFF -DKAHYPAR_DISABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
-make -j"$(get_num_cores)" install.mtkahypar
+make -j$(nproc) install.mtkahypar
 cd ${ROOT}
 
 # build SharedMap
@@ -24,9 +20,6 @@ rm -rf build
 mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release
-cmake --build . --parallel "$(get_num_cores)" --target SharedMap # the executable
-cmake --build . --parallel "$(get_num_cores)" --target sharedmap # the library
+cmake --build . --parallel $(nproc) --target SharedMap # the executable
+cmake --build . --parallel $(nproc) --target sharedmap # the library
 cd ${ROOT}
-
-mkdir -p bin
-mv build/SharedMap bin/SharedMap
