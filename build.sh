037950b64922cb7c38298593ad585e9b9682c5dd
henning <henning.woydt@gmail.com>
2025-01-31 10:00:06 +0100

    * simplified build script

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ROOT=${PWD}
 if [ -d .git ]; then
 #  SharedMap is build from a git repository
   git submodule update --init --recursive
@@ -12,12 +11,11 @@ mkdir -p extern/mt-kahypar/build
 cd extern/mt-kahypar/build
 cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DKAHYPAR_DOWNLOAD_TBB=ON -DKAHYPAR_DOWNLOAD_BOOST=ON -DKAHYPAR_ENABLE_THREAD_PINNING=OFF -DKAHYPAR_DISABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
 make -j$(nproc) install.mtkahypar
-cd ${ROOT}
+cd ../../..
 
 # build SharedMap
-rm -rf build
-mkdir -p build
+rm -rf build && mkdir -p build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSHAREDMAP_DOWNLOAD_TBB=ON
 cmake --build . --parallel $(nproc) --target SharedMap # the executable
 cmake --build . --parallel $(nproc) --target sharedmap # the library
