aab78a5779ffdde23a9db40062f6f5a75a92083c
henning <henning.woydt@gmail.com>
2025-05-20 11:21:58 +0200

    * build script does not use all available cores, but only 8

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+MAX_THREADS=8  # Set the desired number of threads here
+
 if [ -d .git ]; then
-#  SharedMap is build from a git repository
+  # SharedMap is built from a git repository
   git submodule update --init --recursive
 fi
 
@@ -9,13 +11,19 @@ fi
 rm -rf extern/mt_kahypar_local && mkdir -p extern/mt_kahypar_local
 rm -rf extern/mt-kahypar/build && mkdir -p extern/mt-kahypar/build
 cd extern/mt-kahypar/build
-cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_BUILD_TYPE=Release -DKAHYPAR_DOWNLOAD_TBB=ON -DKAHYPAR_DOWNLOAD_BOOST=ON -DKAHYPAR_ENABLE_THREAD_PINNING=OFF -DKAHYPAR_DISABLE_ASSERTIONS=ON -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
-make -j install.mtkahypar
+cmake .. -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+         -DCMAKE_BUILD_TYPE=Release \
+         -DKAHYPAR_DOWNLOAD_TBB=ON \
+         -DKAHYPAR_DOWNLOAD_BOOST=ON \
+         -DKAHYPAR_ENABLE_THREAD_PINNING=OFF \
+         -DKAHYPAR_DISABLE_ASSERTIONS=ON \
+         -DCMAKE_INSTALL_PREFIX=$(pwd)/../../mt_kahypar_local
+make -j"$MAX_THREADS" install.mtkahypar
 cd ../../..
 
 # build SharedMap
 rm -rf build && mkdir -p build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DSHAREDMAP_DOWNLOAD_TBB=ON
-cmake --build . --parallel --target SharedMap # the executable
-cmake --build . --parallel --target sharedmap # the library
+cmake --build . --parallel "$MAX_THREADS" --target SharedMap  # the executable
+cmake --build . --parallel "$MAX_THREADS" --target sharedmap  # the library
